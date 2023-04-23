25450dac4ab3d9712885cae4a2fdbc69bcd58ed0
Henrique Vilela <hvilelaeng@gmail.com>
2023-04-23 12:54:07 -0300

    force card order asc,empty description to new card

--- src/pages/api/tasks.js
+++ src/pages/api/tasks.js
@@ -6,20 +6,29 @@ const prisma = new PrismaClient()
 export default async function handler(req, res) {
 
     if (req.method === 'GET') {
-        const tasks = await prisma.task.findMany()
+        const tasks = await prisma.task.findMany(
+            {
+                orderBy: {
+                    id: 'asc'
+                }
+            }
+        )
         if (!tasks) return res.status(404).json({ message: 'Tasks not found' })
 
         res.status(200).json(tasks)
     }
 
     else if (req.method === 'POST') {
+
         const task = await prisma.task.create({
             data: {
                 title: req.body.title,
-                projectId: req.body.projectId
+                projectId: req.body.projectId,
+                description: ''
             }
         })
         res.status(201).json(task)
 
     }
+
 }

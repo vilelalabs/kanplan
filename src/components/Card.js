37c7e6a7b77f4f1c6423a0d8ddbabc066897b59d
Henrique Vilela <hvilelaeng@gmail.com>
2023-04-23 01:21:15 -0300

    ident

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,11 +8,11 @@ export default function Card(props) {
                         flex flex-col items-centerjustify-center
                         w-96 bg-gray-100 bg-opacity-95 text-black rounded-lg
                         p-10 m-10 shadow-md gap-4">
-                            <div>
-            <h3 className="text-lg font-bold ">{props.task.title}</h3>
-            <GrClose className="absolute top-4 right-4 text-2xl hover:text-red-400 hover:scale-125 transition duration-200 ease-in-out"
-                onClick={props.closeCard}
-            />
+            <div>
+                <h3 className="text-lg font-bold ">{props.task.title}</h3>
+                <GrClose className="absolute top-4 right-4 text-2xl hover:text-red-400 hover:scale-125 transition duration-200 ease-in-out"
+                    onClick={props.closeCard}
+                />
             </div>
             <p className="text-lg font-normal mt-10"
             > {props.task.description}</p>

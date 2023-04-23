a9213cdb815582238337ed0688df01591d64af03
Henrique Vilela <hvilelaeng@gmail.com>
2023-04-23 00:48:30 -0300

    refactor

--- src/pages/api/tasks.js
+++ src/pages/api/tasks.js
@@ -4,8 +4,22 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async function handler(req, res) {
-    const tasks = await prisma.task.findMany()
-    if(!tasks) return res.status(404).json({ message: 'Tasks not found' })
-    
-    res.status(200).json(tasks)
+
+    if (req.method === 'GET') {
+        const tasks = await prisma.task.findMany()
+        if (!tasks) return res.status(404).json({ message: 'Tasks not found' })
+
+        res.status(200).json(tasks)
+    }
+
+    else if (req.method === 'POST') {
+        const task = await prisma.task.create({
+            data: {
+                title: req.body.title,
+                projectId: req.body.projectId
+            }
+        })
+        res.status(201).json(task)
+
+    }
 }
--- src/services/get-tasks.js
+++
@@ -1,6 +0,0 @@
-
-export default async function getTasks() {
-    const res = await fetch('/api/tasks')
-    const data = await res.json()
-    return data
-  }

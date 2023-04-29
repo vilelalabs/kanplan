8dd127e7cd15bb8b9bec88d7bc50276b77172d07
Henrique Vilela <hvilelaeng@gmail.com>
2023-04-29 11:27:59 -0300

    created different pages  authorized user or not

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,31 @@ import { getProjects, postProjects } from "../services/projects"
 import Header from "@/components/Header"
 
 export default function Home() {
+  const [session, setSession] = useState(false)
+  return (
+    <div>
+      <Head>
+        <title>Kanplan | Home</title>
+      </Head>
+      {session?User():Guest()}
+    </div>
+  )
+}
+
+
+//Guest
+function Guest() {
+  return (
+    <main className="container mx-auto text-center py-20 h-screen">
+      <h3 className="text-4xl font-bold">Guest Homepage</h3>
+      <div className="flex justify-center">
+        <Link href={'/login'} className='mt-5 px-10 py-1 rounded-sm bg-blue-700 text-gray-50' >Sign In</Link>
+      </div>
+    </main>
+  )
+}
+//Authorized User
+function User() {
   const [projects, setProjects] = useState([])
   const [newProjectTitle, setNewProjectTitle] = useState("")
 
@@ -28,32 +53,45 @@ export default function Home() {
     }
   }
 
-
   return (
     <div>
       <Head>
         <title>Kanplan | Home</title>
       </Head>
-    <main className="flex min-h-screen flex-col items-center space-y-16 p-24">
-      <Header />
-      <div>
-        <ul className=" text-2xl flex flex-col items-center justify-center space-y-4">
-          {projects.map((project, key) => (
-            <li key={key} className="flex items-center justify-center space-x-4">
-              <Link href={`/dashboard`} onClick={() => localStorage.setItem('project', project.id)}>
-                {project.title}
-              </Link>
-            </li>
-          ))}
-          <input className="text-center bg-transparent border-gray-200 text-gray-100"
-            type="text" placeholder="Name Your New Project"
-            value={newProjectTitle}
-            onChange={(e) => setNewProjectTitle(e.target.value)}
-            onKeyDown={handleAddNewProject}
-          />
-        </ul>
+      <div className="container mx-auto text-center py-10">
+        <h3 className="text-4xl font-bold">Auth User Homepage</h3>
+        <div className="details">
+          <h5>Unknown</h5>
+          <h5>Unknown</h5>
+        </div>
+
+        <div className="flex justify-center">
+          <button className="mt-5 px-10 py-1 rounded-sm bg-blue-700 text-gray-50">Sign Out</button>
+        </div>
+        <div className="flex justify-center">
+          <Link href={'/profile'} className='mt-5 px-10 py-1 rounded-sm bg-blue-700 text-gray-50' >Profile</Link>
+        </div>
       </div>
-    </main>
+      <main className="flex min-h-screen flex-col items-center space-y-16 p-24">
+        <Header />
+        <div>
+          <ul className=" text-2xl flex flex-col items-center justify-center space-y-4">
+            {projects.map((project, key) => (
+              <li key={key} className="flex items-center justify-center space-x-4">
+                <Link href={`/dashboard`} onClick={() => localStorage.setItem('project', project.id)}>
+                  {project.title}
+                </Link>
+              </li>
+            ))}
+            <input className="text-center bg-transparent border-gray-200 text-gray-100"
+              type="text" placeholder="Name Your New Project"
+              value={newProjectTitle}
+              onChange={(e) => setNewProjectTitle(e.target.value)}
+              onKeyDown={handleAddNewProject}
+            />
+          </ul>
+        </div>
+      </main>
     </div>
   )
 }

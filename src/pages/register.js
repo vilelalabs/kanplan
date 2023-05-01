a114a10638afd81f91ac5ee5d67d835babfd0487
Henrique Vilela <hvilelaeng@gmail.com>
2023-05-01 00:19:35 -0300

    changes style of errors on register fields

--- src/pages/register.js
+++ src/pages/register.js
@@ -39,7 +39,7 @@ export default function Register() {
                 {/*form*/}
                 <form className="flex flex-col gap-5" onSubmit={formik.handleSubmit}>
                     <h1 className="text-2xl font-bold text-center"> Register </h1>
-                    <div className={styles.input_group}>
+                    <div className={`${styles.input_group} ${formik.errors.username && formik.touched.username?'border-rose-600':''}`}>
                         <input
                             type="text"
                             name="username"
@@ -51,9 +51,8 @@ export default function Register() {
                             <HiOutlineUser size={25} />
                         </span>
                     </div>
-                    {formik.errors.username && formik.touched.username? <span className="text-red-500 text-sm">{formik.errors.username}</span> : null}
-                    <div className={styles.input_group}>
-                        <input
+                    {/* {formik.errors.username && formik.touched.username? <span className="text-red-500 text-sm">{formik.errors.username}</span> : null} */}
+                    <div className={`${styles.input_group} ${formik.errors.email && formik.touched.email?'border-rose-600':''}`}>                        <input
                             type="email"
                             name="email"
                             placeholder="email"
@@ -64,9 +63,8 @@ export default function Register() {
                             <HiAtSymbol size={25} />
                         </span>
                     </div>
-                    {formik.errors.email && formik.touched.email? <span className="text-red-500 text-sm">{formik.errors.email}</span> : null}
-                    <div className={styles.input_group}>
-                        <input
+                    {/* {formik.errors.email && formik.touched.email? <span className="text-red-500 text-sm">{formik.errors.email}</span> : null} */}
+                    <div className={`${styles.input_group} ${formik.errors.password && formik.touched.password?'border-rose-600':''}`}>                        <input
                             type={showPassword.password ? "text" : "password"}
                             name="password"
                             placeholder="password"
@@ -79,9 +77,8 @@ export default function Register() {
                             <HiFingerPrint size={25} />
                         </span>
                     </div>
-                    {formik.errors.password && formik.touched.password? <span className="text-red-500 text-sm">{formik.errors.password}</span> : null}
-                    <div className={styles.input_group}>
-                        <input
+                    {/* {formik.errors.password && formik.touched.password? <span className="text-red-500 text-sm">{formik.errors.password}</span> : null} */}
+                    <div className={`${styles.input_group} ${formik.errors.cpassword && formik.touched.cpassword?'border-rose-600':''}`}>                        <input
                             type={showPassword.cpassword ? "text" : "password"}
                             name="cpassword"
                             placeholder="confirm password"
@@ -94,7 +91,7 @@ export default function Register() {
                             <HiFingerPrint size={25} />
                         </span>
                     </div>
-                    {formik.errors.cpassword && formik.touched.cpassword? <span className="text-red-500 text-sm">{formik.errors.cpassword}</span> : null}
+                    {/* {formik.errors.cpassword && formik.touched.cpassword? <span className="text-red-500 text-sm">{formik.errors.cpassword}</span> : null} */}
                     {/*sign up buttons*/}
                     <div className="input-button">
                         <button type="submit" className={styles.button}>

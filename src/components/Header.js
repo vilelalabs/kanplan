9e34381beeba8368d763f9c96d66099efdf2ed44
Henrique Vilela <hvilelaeng@gmail.com>
2023-05-06 04:05:10 -0300

    few css corrections

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ export default function Header() {
     return (
         <>
             <div className="flex flex-col items-center gap-2 m-4">
-                <h1 className="text-6xl font-bold text-center"> KANPLAN </h1>
+                <h1 className="text-4xl sm:text-6xl font-bold text-center"> KANPLAN </h1>
                 <p className="text-lg italic text-center">"Experience minimalism in project management, tailored just for you."</p>
             </div>
         </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -66,7 +66,7 @@ function User({ session, handleSignOut }) {
       </Head>
 
       <div className="flex justify-between items-center text-center mx-4 py-2">
-      <Link href={'/'}><img src='/assets/logo.png' className="w-35 h-20" /></Link>
+      <Link href={'/'}><img src='/assets/logo.png' className="sm:w-35 sm:h-20" /></Link>
         <div className="flex flex-row items-center">
         <div className="details flex flex-col md:flex-row gap-2">
           <h5>Hi, {session.user.name}!</h5>
@@ -81,7 +81,7 @@ function User({ session, handleSignOut }) {
           <Link href={'/profile'} className='mt-5 px-10 py-1 rounded-sm bg-blue-700 text-gray-50' >Profile</Link>
         </div> */}
       </div>
-      <main className="flex min-h-screen flex-col items-center space-y-16 p-24">
+      <main className="flex min-h-screen flex-col items-center space-y-16 sm:pt-24">
         <Header />
         <div>
           <ul className=" text-2xl flex flex-col items-center justify-center space-y-4">

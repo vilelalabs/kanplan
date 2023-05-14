f4e699bab3cf03c4864d71d65056e4a607822c22
Henrique Vilela <hvilelaeng@gmail.com>
2023-05-14 02:15:09 -0300

    center text on achived projects banner

--- src/components/BannerArchivedProjects.js
+++ src/components/BannerArchivedProjects.js
@@ -5,7 +5,7 @@ import { FaExclamation } from "react-icons/fa";
 export default function BannerArchivedProjects(props) {
 
     return (
-        <div className="w-screen flex items-center justify-center bg-gray-500 bg-opacity-40 text-lg font-bold px-4 py-3">
+        <div className="w-screen flex items-center text-center justify-center bg-gray-500 bg-opacity-40 text-lg font-bold px-4 py-3">
             "Attention! You are viewing archived project(s)."
         </div>
     )
